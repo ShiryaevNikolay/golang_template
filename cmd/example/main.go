9a4732835b18b6c45cb4f60446e511eeff46cae5
Nikolay Shiryaev <deendeeps@list.ru>
2021-12-05 18:21:16 +0300

    fix config usage

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -39,8 +39,10 @@ func main() {
 	// которая находится в файле handler.go
 	r.Get("/hello", h.Hello)
 
-	log.Print("starting server")
-	err = http.ListenAndServe(":8080", r) // Возвращает ошибку
+	path := cfg.Host+":"+cfg.Port
+
+	log.Printf("starting server at %s", path)
+	err = http.ListenAndServe(path, r) // Возвращает ошибку
 	log.Fatal(err)
 	log.Print("shutting server down")
 }
