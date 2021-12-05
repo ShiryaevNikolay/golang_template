a8c2c483acbab15b3c18b335e63b44b72add9f4c
Nikolay Shiryaev <deendeeps@list.ru>
2021-12-05 14:06:47 +0300

    слушает запрос GET

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"example/internal/handler"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
-	
+
+	h :=  handler.NewHandler() // handler, который будет слушать запросы
+
+	r := chi.NewRouter() // Создается роутер. Он обрабатывает запросы (GET, POST, DELETE...)
+
+	// Запрос типа GET.
+	//
+	// Первым параметром принимает в себя путь. 
+	// Второй параметр - функция handler, 
+	// который будет слушать этот запрос. В данном случае это функция Hello, 
+	// которая находится в файле handler.go
+	r.Get("/hello", h.Hello)
 }
